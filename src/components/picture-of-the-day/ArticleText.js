d882bd93f372356a00c78015b9a42003c8bb0370
Andrew <script252@gmail.com>
2019-11-10 20:54:45 -0600

    Added toggle to article text.

--- src/components/picture-of-the-day/ArticleText.js
+++ src/components/picture-of-the-day/ArticleText.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ArticleText.scss';
 
-function ArticleText({title, subTitle, text}) {
+function ArticleText({title, subTitle, text, startVisible=false}) {
+
+    const [visible, setVisible] = useState(startVisible);
+
+    const onClickTab = () => { setVisible(!visible) };
+
     return (
-        <div className = "article-text">
+        <div className = {visible ? "article-text opened" : "article-text"} onClick={onClickTab}>
             <h2>{subTitle}</h2>
             <h1>{title}</h1>
             <p>{text}</p>
